9106da7c210c6617bb7e7ad4be374b32f9847807
Polpsi <59441238+Polpsi@users.noreply.github.com>
2020-04-12 13:09:15 +0300

    renamefile

--- src/main/java/Lesson_3/Read10MbFromFile/src/MainRead10Mb.java
+++ src/main/java/Lesson_3/Read10MbFromFile/src/MainRead10Mb.java
@@ -3,7 +3,7 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class Read10Mb {
+public class MainRead10Mb {
 
     public static void main(String[] args) {
         Scanner getFilePath = new Scanner(System.in);
